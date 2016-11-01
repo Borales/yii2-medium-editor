c930d4ce0c0fccf24c4a26d48ec85529693bccf6
Borales <bordun.alexandr@gmail.com>
2016-11-01 18:18:42 +0100

    Adding AssetBundle

---
+++ src/Asset.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace borales\medium;
+
+use yii\web\AssetBundle;
+
+/**
+ * @package borales\medium
+ */
+class Asset extends AssetBundle
+{
+    /**
+     * @var bool Whether to use "latest" version from CDN
+     */
+    public $useLatest = true;
+
+    /**
+     * @var string if $useLatest is set to "false" - use this version
+     */
+    public $cdnVersion = '5.22.1';
+
+    /**
+     * @var array
+     */
+    public $css = [
+        'css/medium-editor.min.css'
+    ];
+
+    /**
+     * @var array
+     */
+    public $js = [
+        'js/medium-editor.min.js'
+    ];
+
+    /**
+     * @var string Medium Editor color theme
+     */
+    public $theme = 'default';
+
+    /**
+     * @var array List of available Medium Editor color themes
+     */
+    protected $availableThemes = [
+        'beagle', 'bootstrap', 'default', 'flat', 'mani', 'roman', 'tim'
+    ];
+
+    /**
+     * @param \yii\web\View $view
+     */
+    public function registerAssetFiles($view)
+    {
+        if(!in_array($this->theme, $this->availableThemes)) {
+            $this->theme = 'default';
+        }
+
+        // Adding selected Medium theme
+        $this->css[] = "css/themes/{$this->theme}.min.css";
+
+        if(!$this->sourcePath) {
+            foreach ($this->js as $jsIndex=>$jsFile) {
+                $this->js[$jsIndex] = $this->getAssetUrl($jsFile);
+            }
+
+            foreach ($this->css as $cssIndex=>$cssFile) {
+                $this->css[$cssIndex] = $this->getAssetUrl($cssFile);
+            }
+        }
+
+        parent::registerAssetFiles($view);
+    }
+
+    /**
+     * Get CDN URL
+     * @param string $file
+     * @return string
+     */
+    protected function getAssetUrl($file)
+    {
+        return sprintf('https://cdn.jsdelivr.net/medium-editor/%s/%s',
+            ($this->useLatest ? 'latest' : $this->cdnVersion ),
+            $file
+        );
+    }
+}
